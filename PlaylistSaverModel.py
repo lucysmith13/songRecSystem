7fe62907509222909bc791a7311d2444c12bcafe
lucysmith13 <lucyannesmith1301@gmail.com>
2025-12-27 14:42:32 +0000

    add runsql func

--- PlaylistSaverModel.py
+++ PlaylistSaverModel.py
@@ -1,1 +1,14 @@
 import spotipy, requests
+import sqlite3 as db
+
+def setup_database():
+    def runsql(*args):
+        conn = db.connect("PlaylistSaver.db")
+        conn.execute("PRAGMA foreign_keys = 1")
+        cursor = conn.cursor()
+        if len(args) == 1:
+            cursor.execute(args[0])
+        else:
+            cursor.execute(args[0], args[1])
+        conn.commit()
+        return cursor.fetchall()
